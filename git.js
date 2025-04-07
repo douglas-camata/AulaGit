521d46b47e41a623da8f26ff937d74677b95cafe
Douglas Camata de Araujo <douglas.camata@gmail.com>
2025-04-07 15:41:56 -0300

    Testes no código

--- git.js
+++ git.js
@@ -1,5 +1,6 @@
 let comidas = ['Lasanha', 'Pizza', 'Sushi', 'Hamburguer'];
 console.log(`Lista da todas as comidas: ${comidas}`);
-console.log(`Primeira comida: ${comidas[0]}`);
+console.log('Primeira comida: ${comidas[0]}');
 comidas.sort();
-console.log(`Comidas ordenadas: ${comidas}`);
+console.log('Comidas ordenadas: ${comidas}');
+console.log('FIM');
