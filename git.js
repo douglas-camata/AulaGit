96c15732b1254004461b9d34f2db755a7985e5ed
Douglas Camata de Araujo <douglas.camata@gmail.com>
2025-04-07 15:18:17 -0300

    Incluído ordenação da lista

--- git.js
+++ git.js
@@ -1,4 +1,5 @@
 let comidas = ['Lasanha', 'Pizza', 'Sushi', 'Hamburguer'];
 console.log(`Lista da todas as comidas: ${comidas}`);
 console.log(`Primeira comida: ${comidas[0]}`);
-
+comidas.sort();
+console.log(`Comidas ordenadas: ${comidas}`);
